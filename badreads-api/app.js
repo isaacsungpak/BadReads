f77eb683de23d132e783fc5cbe9268e9e66a3ca5
jaguitart <jaguitart@uc.cl>
2021-11-16 09:46:24 -0800

    books route

--- badreads-api/app.js
+++ badreads-api/app.js
@@ -8,6 +8,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const booksRouter = require('./routes/books');
 const { restoreUser, requireAuth } = require('./auth');
 const {sessionSecret} = require('./config');
 const csrf = require('csurf')
@@ -42,6 +43,7 @@ store.sync();
 app.use(restoreUser);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/books', booksRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
