a8218fa14b10e892817b81a7e3fbe40e5bc2c733
Ryan Bender <ryan_bender@ymail.com>
2022-03-25 10:33:02 -0500

    removed ssl

--- config/index.js
+++ config/index.js
@@ -1,7 +1,6 @@
 module.exports = {
   environment: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 8080,
-  ssl: { rejectUnathorized: false},
   db: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
